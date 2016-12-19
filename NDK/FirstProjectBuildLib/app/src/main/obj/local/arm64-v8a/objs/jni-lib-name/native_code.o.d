764fda6e432c5a1e7f4653bd3dd9fdb703427956
Nguyễn Văn Ý <mry.itvnn@gmail.com>
2016-12-19 17:08:38 +0700

    ..

---
+++ NDK/FirstProjectBuildLib/app/src/main/obj/local/arm64-v8a/objs/jni-lib-name/native_code.o.d
@@ -0,0 +1,2 @@
+/Users/HN/Desktop/Android-Self-Study/NDK/FirstProjectBuildLib/app/src/main//obj/local/arm64-v8a/objs/jni-lib-name/native_code.o: \
+  /Users/HN/Desktop/Android-Self-Study/NDK/FirstProjectBuildLib/app/src/main//jni/native_code.c
